95a93cb14c59db4223f730aa1020a6383776d375
tenzin789 <tenzinthayai@gmail.com>
2019-02-10 14:16:24 -0500

    To-Do List App
    To-Do List App created with html, css and JQuery

---
+++ assets/js/todos.js
@@ -0,0 +1,30 @@
+// Check off Specific Todos by Clicking
+$("ul").on("click", "li", function() {
+    $(this).toggleClass("completed")
+});
+
+// Click on X to delete Todo
+$("ul").on("click", "span", function(event) {
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+    if(event.which === 13) {
+        //grabbing new todo text from input
+        var todoText = $(this).val();
+        $(this).val("");
+        if(todoText === "") {
+            alert("Please enter a value and try again.");
+        } else {
+        //create a new li and add to ul
+        $("ul").append("<li><span><i class='far fa-trash-alt'></i></span> "+ todoText + "</li>");
+        }
+    }
+});
+
+$(".fa-plus").click(function() {
+    $("input[type='text']").fadeToggle();
+});
